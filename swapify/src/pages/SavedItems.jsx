17fc3377c6722bd4e9da21f06a9534c1581d9144
aisharoslan <ar7805@nyu.edu>
2026-03-18 03:18:40 -0400

    update home snapshot

--- swapify/src/pages/SavedItems.jsx
+++ swapify/src/pages/SavedItems.jsx
@@ -1,13 +1,15 @@
 import Navbar from '../components/Navbar'
-import '../styles/savedItems.css'
+import '../styles/savedItems.css';
 
 const SavedItems = () => {
-  return (<>
-    <Navbar />
+  return (
     <div>
-      <h1 className="saved-items-title">Your Saved Items</h1>
+      <Navbar />
+      <div>
+        <h1 className="saved-items-title">Your Saved Items</h1>
+      </div>
     </div>
-  </>)
+  )
 }
 
 export default SavedItems
